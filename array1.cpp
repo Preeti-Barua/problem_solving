8473044db1adc23ca0e6e9b35e03b143191d87a4
Preeti-Barua <40754414+Preeti-Barua@users.noreply.github.com>
2020-02-23 19:56:34 +0530

    sum of an array elements

---
+++ array1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{                     //Given an array of integers, find the sum of its elements 
+                    int arr[]={1,2,3,4,5};
+	int sum=0;
+	for(int i=0;i<arr[4];i++)
+	{  
+	     sum=sum+arr[i];
+	}
+	cout<<sum;
+	return 0;
+}          
+
