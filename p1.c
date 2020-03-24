7f9a51c72baf1ea6881e32710de8dd80249b59f9
Preeti-Barua <40754414+Preeti-Barua@users.noreply.github.com>
2020-03-24 21:30:30 +0530

    table

---
+++ p1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,n,a;	
+	for(n=1;n<=20;n++)
+	{
+		for(i=1;i<=10;i++)
+		{
+  			a = n*i;
+
+			printf("%d\t",a);
+		
+
+		}
+		printf("\n");
+	}
+return 0;
+}
+  
