f62cd6574e52971fd35e7e35c09a7572901729fa
Preeti-Barua <40754414+Preeti-Barua@users.noreply.github.com>
2020-03-27 22:48:33 +0530

    27/03/2020
    constructor

---
+++ B.java
@@ -0,0 +1,19 @@
+class Bike
+{
+    bike()
+    {
+        System.out.println("hello");
+    }
+public  static void main(String args[]) {
+
+
+
+    
+
+
+
+
+    Bike b1=new Bike();
+    
+}
+}
---
+++ Bike.java
@@ -0,0 +1,20 @@
+//constructor
+class Bike
+{
+    Bike()
+    {
+        System.out.println("hello");
+    }
+public  static void main(String args[]) {
+
+
+
+    
+
+
+
+
+    Bike b1=new Bike();
+    
+}
+}
