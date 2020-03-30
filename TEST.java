d792cb858f328f5956452669bda79d6b803c6d43
Preeti-Barua <40754414+Preeti-Barua@users.noreply.github.com>
2020-03-30 22:38:41 +0530

    30/03/2020

---
+++ TEST.java
@@ -0,0 +1,18 @@
+class TEST
+{
+    static int x = 2;
+
+    static void a()
+    {
+        System.out.println("x="+x);
+    }
+    
+}
+class Demo
+{
+    public static void main(String args[]) 
+    {
+    TEST.a();
+        
+    }
+}
