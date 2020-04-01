81e510860dc9ec16636455376989617bc7f69f16
Preeti-Barua <40754414+Preeti-Barua@users.noreply.github.com>
2020-04-01 23:54:30 +0530

    01/04/2020

---
+++ Test5.java
@@ -0,0 +1,19 @@
+class Test5
+{
+    static int add(int a,int b)
+    {
+        return a+b;
+    }
+    static int add(int a ,int b,int c)
+    {
+        return a+b+c;
+    }
+}
+class Demo{
+    public static void  main(String args[])
+    {
+        System.out.println(Test5.add(2,3));
+        System.out.println(Test5.add(2,3,4));
+        
+    }
+}
