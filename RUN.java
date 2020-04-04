2068dbc6a65c3f0ac809a1b81b0d25eb63a98251
Preeti-Barua <40754414+Preeti-Barua@users.noreply.github.com>
2020-04-04 21:53:55 +0530

    04/04/2020

---
+++ RUN.java
@@ -0,0 +1,21 @@
+class RUN
+{
+    void run()
+    {
+        System.out.println(" waiting!!!!!!!! stupid AC");
+    }
+
+}
+class runs extends RUN{
+    void run()
+    {
+        System.out.println(" tring tring!!!!");
+    }
+
+    public static void main(String args[])
+    {
+        RUN r=new runs();
+        r.run();
+        
+    }
+}
